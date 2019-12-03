23804280a464bed773aec0797e3f74c686ac9dd2
leonardo lima <leo.lima21051990@gmail.com>
2019-12-03 13:59:00 -0300

    show RGB plot

--- src/cv2test.py
+++ src/cv2test.py
@@ -28,8 +28,21 @@ class camera:
 		while True:
 			ret, cv2_frame = self.cap.read()
 			cv2.circle(cv2_frame,(20, 20), 5, (0,255,0), -1)
+
+			cv2.namedWindow('r', cv2.WINDOW_NORMAL)
+			cv2.namedWindow('g', cv2.WINDOW_NORMAL)
+			cv2.namedWindow('b', cv2.WINDOW_NORMAL)
+			# show rgb channels
+			cv2.imshow('b', cv2_frame[:, :, 0])
+			cv2.imshow('g', cv2_frame[:, :, 1])
+			cv2.imshow('r', cv2_frame[:, :, 2])
+			
+			if cv2.waitKey(1) == ord('q'):
+				break
+
 			ros_frame = self.bridge.cv2_to_imgmsg(cv2_frame, "bgr8")
 			self.pub.publish(ros_frame)
+
 
 # main function
 if __name__	== '__main__':
