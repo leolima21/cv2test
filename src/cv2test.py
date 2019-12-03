5c9d005b6311ad633c9a84ed24d981a4042d0928
leonardo lima <leo.lima21051990@gmail.com>
2019-12-03 16:27:16 -0300

    circle detection

--- src/cv2test.py
+++ src/cv2test.py
@@ -27,33 +27,45 @@ class camera:
 	def pub_img(self):
 		while True:
 			ret, cv2_frame = self.cap.read()
-			cv2.circle(cv2_frame,(20, 20), 5, (0,255,0), -1)
 
-			cv2.namedWindow('r', cv2.WINDOW_NORMAL)
-			cv2.namedWindow('g', cv2.WINDOW_NORMAL)
-			cv2.namedWindow('b', cv2.WINDOW_NORMAL)
-			cv2.namedWindow('x', cv2.WINDOW_NORMAL)
+			cv2_frame = cv2.medianBlur(cv2_frame, 5)
+			# cimg = cv2.cvtColor(cv2_frame, cv2.COLOR_GRAY2BGR)
+			gray = cv2.cvtColor(cv2_frame, cv2.COLOR_BGR2GRAY)
+
+
+			circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,1000, param1=30,param2=50,minRadius=20,maxRadius=150)
+
+			circles = np.uint16(np.around(circles))
 			
-			# rgb var
-			r = cv2_frame[:, :, 2]
-			g = cv2_frame[:, :, 1]
-			b = cv2_frame[:, :, 0]
-
-			# effects
-			b[(b > 100) & (g < 200)] = 255
-			b[b < 255] = 0
-
-			# merge rgb channels
-			x = cv2_frame
-			x[:,:,0] = b
-			x[:,:,1] = g
-			x[:,:,2] = r
+			for i in circles[0,:]:
+				cv2.circle(cv2_frame,(i[0],i[1]), i[2], (0,255,0),2)
+				cv2.circle(cv2_frame,(i[0],i[1]), 2, (0,0,255),3)
+			cv2.imshow('detected circles', cv2_frame)
+			# cv2.namedWindow('r', cv2.WINDOW_NORMAL)
+			# cv2.namedWindow('g', cv2.WINDOW_NORMAL)
+			# cv2.namedWindow('b', cv2.WINDOW_NORMAL)
+			# cv2.namedWindow('x', cv2.WINDOW_NORMAL)
 			
-			# show rgb channels
-			cv2.imshow('b', b)
-			cv2.imshow('g', g)
-			cv2.imshow('r', r)
-			cv2.imshow('x', x)
+			# # rgb var
+			# r = cv2_frame[:, :, 2]
+			# g = cv2_frame[:, :, 1]
+			# b = cv2_frame[:, :, 0]
+
+			# # effects
+			# b[(b > 100) & (g < 200)] = 255
+			# b[b < 255] = 0
+
+			# # merge rgb channels
+			# x = cv2_frame
+			# x[:,:,0] = b
+			# x[:,:,1] = g
+			# x[:,:,2] = r
+			
+			# # show rgb channels
+			# cv2.imshow('b', b)
+			# cv2.imshow('g', g)
+			# cv2.imshow('r', r)
+			# cv2.imshow('x', x)
 			
 			if cv2.waitKey(1) == ord('q'):
 				break
